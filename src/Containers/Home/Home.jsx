ccfa9cefe96697a1c55848707b78c39e990d631c
t.zemmari <tarik.zemmari@xabec.es>
2021-04-08 21:33:38 +0200

    home scss modified

--- src/Containers/Home/Home.jsx
+++ src/Containers/Home/Home.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
+import Footer from '../../components/Footer/Footer';
 import Header from '../../components/Header/Header';
 import tablet from '../../img/Tablet.jpg';
+import accesorios from '../../img/accesories.png';
+import smartphone from '../../img/Smartphones.jpg';
+import repuestos from '../../img/reparar.png';
 import './Home.scss';
 
 
@@ -20,11 +24,41 @@ const Home =(props)=>{
             <img className="vista-portada-uno" src={tablet} alt="tab"/>
             <div className="header"><Header/></div>
         </div>
-        <div className="vista-contenedor-resp-smart-acces">
-           <div className="vista-contenido"></div>
-           <div className="vista-contenido"></div>
-           <div className="vista-contenido"></div>
+        <div className='separador'></div>
+
+        <div className="vista-contenedor-rep-smart-acces">
+
+           <div className="vista-contenedor-divs-rep-smart-acces">
+           <div className="vista-contenido">
+              <img className="vista-contenido" src={smartphone} alt="smart"/>
+           </div> 
+           <p className='p-home-titulo'>Telefonos</p> 
+           <p className='p-home-parrafo'>Navega por nuestro catalogo, y elige el accesorio que <br/>
+           mas te gusta, a un precio de chollo.</p> 
+           </div>
+
+           <div className="vista-contenedor-divs-rep-smart-acces">
+           <div className="vista-contenido">
+              <img className="vista-contenido" src={repuestos} alt="repuestos"/>
+           </div>
+           <p className='p-home-titulo'>Repuestos</p>
+           <p className='p-home-parrafo'>Navega por nuestro catalogo, y elige el accesorio que <br/>
+           mas te gusta, a un precio de chollo.</p>
+           </div>
+
+           <div className="vista-contenedor-divs-rep-smart-acces">
+           <div className="vista-contenido">
+              <img className="vista-contenido" src={accesorios} alt="accesorios"/>
+           </div> 
+           <p className='p-home-titulo'>Accesorios</p>
+           <p className='p-home-parrafo'>Navega por nuestro catalogo, y elige el accesorio que <br/>
+           mas te gusta, a un precio de chollo.</p>
+           </div>
         </div>
+        
+
+        <div className="footer"><Footer/></div>
+        
     </div>
    )
 
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import './Footer.scss';
+
+
+const Footer =(props)=>{
+
+
+
+   return(
+
+
+    <div className="footer-container">
+        <div className="vista-sobre-nosotros">
+            <p>Enlaces de interes</p>
+            <div>Envios</div>
+            <div>Repuestos</div>
+            <div>Accesorios</div>
+            <div>Telefonos nuevos y de segunda mano</div>
+            <div>Copyright TEcMovil</div>
+        
+        </div>
+        <div className="vista-sobre-nosotros">
+            <p>Sobre Nosotros</p>
+            <div>Calle los leones 28 bajo 46022 Valencia</div>
+            <div></div>
+            <div></div>
+        </div>
+
+        <div className="vista-sobre-nosotros">
+            <p>Sobre Nosotros</p>
+            <div>Calle los leones 28 bajo 46022 Valencia</div>
+            <div></div>
+            <div></div>
+        </div>
+
+    </div>
+   )
+
+}
+
+export default Footer;
