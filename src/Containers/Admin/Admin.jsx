b324364db81c37a8eb4f17f14ccf30c2efb8d2f0
t.zemmari <tarik.zemmari@xabec.es>
2021-04-25 20:47:49 +0200

    admin modified , now we can see products info

--- src/Containers/Admin/Admin.jsx
+++ src/Containers/Admin/Admin.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import './Admin.scss';
 import Header from '../../components/Header/Header';
+import ProductsAdmin from '../../components/Products-Admin/Products-admin';
 import Button from '@material-ui/core/Button';
 import User from '../../components/User/User';
 import axios from 'axios';
@@ -10,11 +11,14 @@ const Admin =(props)=>{
 
   const [usuario,setUsuario]=useState([]);
   const [loading,setLoading]=useState(false);
+  const [products,setProducs]=useState([]);
 
   let UserEndpoint='http://localhost:3002/users';
+  let ProductEndpoint = 'http://localhost:3002/products';
 
   useEffect(async ()=>{
-      setUsuario([])
+      setUsuario([]);
+      setProducs([]);
   },[])
 
   const listaUsuarios =async ()=>{
@@ -22,6 +26,11 @@ const Admin =(props)=>{
    let userData= await axios.get(UserEndpoint);
    setUsuario(userData.data) ;
    
+  }
+  const listaProductos = async()=>{
+    let productsData = await axios.get(ProductEndpoint);
+    setProducs(productsData.data);
+    console.log(products)
   }
   
  
@@ -36,7 +45,7 @@ const Admin =(props)=>{
             {/*--------------------Menu Izquierdo------------------------- */}
             
              <div className="vista-menu-izquierda-admin">
-               <div className="boton-admin-menu-izquierdo"> <Button variant="contained" color="primary">
+               <div className="boton-admin-menu-izquierdo"> <Button variant="contained" color="primary" onClick ={()=>{listaProductos()}}>
                  Lista D Productos
                </Button>
                </div>
@@ -72,7 +81,6 @@ const Admin =(props)=>{
                
                <div className="lista-usuarios-registrados">
                    <div className=" user-data-admin user-name-admin-view">Nombre</div>
-                   
                    <div className="user-data-admin  user-lastname-admin-view">Apellidos</div>
                    <div className="user-data-admin user-email-admin-view">Email</div>
                    <div className="user-data-admin user-phone-admin-view">Telefono</div>
@@ -89,9 +97,23 @@ const Admin =(props)=>{
                 {/*---------------------Productos Menu Central-----------------------*/}
 
                 <div className="lista-productos">
-
+                <h2>Lista De Los Productos Registrados</h2>
 
                 </div>
+
+                <div className="lista-productos-registrados">
+                   <div className="product-data-admin product-identificador-admin-view">Identificador</div>
+                   <div className="product-data-admin product-name-admin-view">Producto</div>
+                   <div className="product-data-admin product-price-admin-view">precio</div>
+                   <div className="product-data-admin product-color-admin-view">Color</div>
+                   <div className="product-data-admin product-rate-admin-view">rate</div>
+                   <div className="product-data-admin product-pantalla-admin-view">Pantalla</div>
+                   <div className="product-data-admin product-image-admin-view">image</div>
+               </div>
+
+               <div>
+                {products?.map(products => <ProductsAdmin key={products._id} {...products}/>)}
+               </div>
 
                  
 
