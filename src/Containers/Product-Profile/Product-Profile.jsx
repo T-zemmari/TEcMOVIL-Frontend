a5c224a6cf6493d6d508207dfd98569d192ab064
t.zemmari <tarik.zemmari@xabec.es>
2021-04-26 21:42:55 +0200

    now we can choose one product and go to it s profile,without login

--- src/Containers/Product-Profile/Product-Profile.jsx
+++ src/Containers/Product-Profile/Product-Profile.jsx
@@ -1,4 +1,7 @@
 import React from 'react';
+import Header from '../../components/Header/Header';
+import Footer from '../../components/Footer/Footer';
+import {Avatar, Button, ClickAwayListener} from '@material-ui/core';
 import './Product-Profile.scss';
 
 
@@ -7,29 +10,134 @@ const ProductProfile = ({tamaño})=>{
    
      let datosProducto = JSON.parse(localStorage.getItem('productos'));
      console.log(datosProducto);
+     let credentials = JSON.parse(localStorage.getItem('credentials'));
 
-    if(datosProducto?.name){
+    if(datosProducto?.name && credentials?.user.name){
+
     return (
-
+        <>
+        <Header style ='logged-two'></Header>
         <div className="vista-product-profile-container">
+
+         <div className="Image-and-datainfo-together-product-profile">
+
           <div className="image-product-profile">
            <img className='img-product-size-on-profile'tamaño ='grande' src={datosProducto.imgUrl} alt={datosProducto.name}/>
           </div>
+
+        
           <div className="data-product-profile">
-              <h1 className='h1-vista-product-profile'>{datosProducto.name}</h1>
+                  <h1 className='h1-vista-product-profile'>{datosProducto.name}</h1>
               <div className="masdatos">
-              <h2 className='h2-vista-product-profile'>Color : {datosProducto.color}</h2>
-              <h2 className='h2-vista-product-profile'>Rate : {datosProducto.rate}</h2>
-              <h2 className='h2-vista-product-profile'>Pantalla : {datosProducto.pantalla} Pulgadas</h2>
-              <h2 className='h2-vista-product-profile'>Precio : {datosProducto.price}</h2>
+                  <div className="vista-color-product-profile">
+                    <h2 className='h2-vista-product-profile'>Color : {datosProducto.color}</h2>
+                  </div>
+                  <div className="vista-rate-product-profile">
+                    <h2 className='h2-vista-product-profile'>Rate : {datosProducto.rate}</h2>
+                  </div>
+                  <div className="vista-pantalla-product-profile">
+                    <h2 className='h2-vista-product-profile'>Pantalla : {datosProducto.pantalla} Pulgadas</h2>
+                  </div>
+                  <div className="vista-price-product-profile">
+                    <h3 className='h3-vista-product-profile'>{datosProducto.price}</h3>
+                  </div>
               </div>
           </div>
+
+          <div className="vista-right-menu-product-profile">
+              <div className="vista-disponible-product-profile">
+                  <h3 className='h3-vista-disponible-product-profile'>Producto Disponible</h3>
+              </div>
+              <div className="vista-garantia-product-profile">
+
+              </div>
+              <div className="vista-entrega-product-profile">
+
+              </div>
+
+              <div className="vista-boton-comprar-product-profile">
+                  <Button variant="contained" color="secondary">
+                      Comprar Ahora
+                  </Button>
+              </div>
+          </div>
+          <div className="vista-description-product-profile">
+          <h2 className='h2-vista-product-profile'>Descripción</h2>
+           <p className='p-vista-product-profile'>{datosProducto.description}</p>
+          </div>
+       </div>
+
+          {/*<div className="vista-description-product-profile">
+          <h2 className='h2-vista-product-profile'>Descripción</h2>
+           <p className='p-vista-product-profile'>{datosProducto.description}</p>
+           </div>*/}
+           <div tamaño ='product-profile'><Footer/></div>
         </div>
-    )}else{
+        </>
+    )}if(datosProducto?.name && !credentials?.user.name){
 
         return(
             <div>
-                Algo esta fallando
+                <>
+        
+        <div className="vista-product-profile-container">
+        <Header style ='register'></Header>
+         <div className="Image-and-datainfo-together-product-profile">
+
+          <div className="image-product-profile">
+           <img className='img-product-size-on-profile'tamaño ='grande' src={datosProducto.imgUrl} alt={datosProducto.name}/>
+          </div>
+
+        
+          <div className="data-product-profile">
+                  <h1 className='h1-vista-product-profile'>{datosProducto.name}</h1>
+              <div className="masdatos">
+                  <div className="vista-color-product-profile">
+                    <h2 className='h2-vista-product-profile'>Color : {datosProducto.color}</h2>
+                  </div>
+                  <div className="vista-rate-product-profile">
+                    <h2 className='h2-vista-product-profile'>Rate : {datosProducto.rate}</h2>
+                  </div>
+                  <div className="vista-pantalla-product-profile">
+                    <h2 className='h2-vista-product-profile'>Pantalla : {datosProducto.pantalla} Pulgadas</h2>
+                  </div>
+                  <div className="vista-price-product-profile">
+                    <h3 className='h3-vista-product-profile'>{datosProducto.price}</h3>
+                  </div>
+              </div>
+          </div>
+
+          <div className="vista-right-menu-product-profile">
+              <div className="vista-disponible-product-profile">
+                  <h3 className='h3-vista-disponible-product-profile'>Producto Disponible</h3>
+              </div>
+              <div className="vista-garantia-product-profile">
+
+              </div>
+              <div className="vista-entrega-product-profile">
+
+              </div>
+
+              <div className="vista-boton-comprar-product-profile">
+                  <Button variant="contained" color="secondary">
+                      Comprar Ahora
+                  </Button>
+              </div>
+          </div>
+          <div className="vista-description-product-profile">
+          <h2 className='h2-vista-product-profile'>Descripción</h2>
+           <p className='p-vista-product-profile'>{datosProducto.description}</p>
+          </div>
+       </div>
+
+          {/*<div className="vista-description-product-profile">
+          <h2 className='h2-vista-product-profile'>Descripción</h2>
+           <p className='p-vista-product-profile'>{datosProducto.description}</p>
+           </div>*/}
+           <div tamaño ='product-profile'><Footer/></div>
+        </div>
+        </>
+                
             </div>
         )
     }
--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -5,11 +5,11 @@ import './Footer.scss';
 const Footer =(props)=>{
 
 
-
+if(props.tamaño ==='product-profile'){
    return(
 
-
-    <div className="footer-container">
+    
+    <div className="footer-container-product-profile">
         <div className="vista-sobre-nosotros">
             <p>Enlaces de interes</p>
             <div>Envios</div>
@@ -34,8 +34,41 @@ const Footer =(props)=>{
         </div>
 
     </div>
+    
    )
+}else {
 
+
+    return(
+
+<div className="footer-container">
+        <div className="vista-sobre-nosotros">
+            <p>Enlaces de interes</p>
+            <div>Envios</div>
+            <div>Repuestos</div>
+            <div>Accesorios</div>
+            <div>Telefonos nuevos y de segunda mano</div>
+            <div>Copyright TEcMovil</div>
+        
+        </div>
+        <div className="vista-sobre-nosotros">
+            <p>Sobre Nosotros</p>
+            <div>Calle los leones 28 bajo 46022 Valencia</div>
+            <div></div>
+            <div></div>
+        </div>
+
+        <div className="vista-sobre-nosotros">
+            <p>Sobre Nosotros</p>
+            <div>Calle los leones 28 bajo 46022 Valencia</div>
+            <div></div>
+            <div></div>
+        </div>
+
+    </div>
+
+    )
+}
 }
 
 export default Footer;
