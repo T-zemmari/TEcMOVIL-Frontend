d81fa22e6e37942c3dc14aa502b1209bf0c2a333
t.zemmari <tarik.zemmari@xabec.es>
2021-04-29 18:00:49 +0200

    now we have products on tienda

--- src/Containers/Tienda/Tienda.jsx
+++ src/Containers/Tienda/Tienda.jsx
@@ -1,6 +1,10 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './Tienda.scss';
 import Header from '../../components/Header/Header';
+import Button from '@material-ui/core/Button';
+import axios from 'axios';
+import Product from '../../components/Products/Product';
+import { useHistory } from 'react-router';
 
 
 
@@ -8,10 +12,31 @@ import Header from '../../components/Header/Header';
 
 const Tienda = (props)=>{
 
+    let history = useHistory();
+
+    const [phones,setPhones]=useState([]);
+
+
+    useEffect(async()=>{
+
+         let response_for_phones = await axios.get('http://localhost:3002/products');
+         setPhones(response_for_phones.data);
+    },[])
+
     let credentials = localStorage.getItem('credentials');
-    let phones = localStorage.getItem('phones');
+    let smartphones = localStorage.getItem('phones');
     let accessories = localStorage.getItem('allAccessories');
     console.log(phones);
+
+
+    const GetProductInfo = (product) => {
+        localStorage.removeItem('productos');
+        localStorage.setItem('productos',JSON.stringify(product));
+        history.push('/product-profile')
+     
+     };
+     
+
 
 
    if(!credentials?.user.name){
@@ -22,15 +47,34 @@ const Tienda = (props)=>{
 
             <div className="nav-bar-container">
            
-            
+             <Button variant="contained" color="secondary">
+                 Smartphones 
+             </Button>
+             <Button variant="contained" color="secondary">
+                 Accesorios 
+             </Button>
+             <Button variant="contained" color="secondary">
+                 Xiaomi
+             </Button>
+             <Button variant="contained" color="secondary">
+                 Samsung
+             </Button>
+             <Button variant="contained" color="secondary">
+                 Iphone
+             </Button>
+
+
+
             </div>
 
           <div className="vista-contenedor-telefonos-repuestos-accessorio">
 
+              <div className="vista-todos-los-Smartphones">
+                  {phones.map(phones => <Product key={phones._id}{...phones} tamaño = 'normal' onClick={()=>GetProductInfo(phones)}/>)}
+              </div>
               <div className="vista-todos-los-accessorios">
 
               </div>
-
 
           </div>
 
