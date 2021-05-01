c6cdc552bc3c5489b8a1f93f5a4782f19c8809fd
t.zemmari <tarik.zemmari@xabec.es>
2021-05-01 15:35:27 +0200

    now you can´t  go to shopping cart if your not logged

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,13 +1,25 @@
+import { Widgets } from '@material-ui/icons';
 import React from 'react';
+import { useHistory } from 'react-router';
 import './Cart.scss';
 
 
 const Cart =({imgUrl,image,name,price,cantidad})=>{
 
+    let history = useHistory();
 
+    let credentials = JSON.parse(localStorage.getItem('credentials'));
     let productsData = localStorage.getItem('productsData');
     console.log(productsData);
-
+    
+    if(!credentials?.user.name){
+       //alert('No puedes acceder al carrito , si no estas logeado')
+     
+        history.push('/')  
+        window.location.reload()
+      
+       
+    }else{
 
 
     return(
@@ -24,6 +36,7 @@ const Cart =({imgUrl,image,name,price,cantidad})=>{
 
         </div>
     )
+    }
 }
 
 export default Cart;
