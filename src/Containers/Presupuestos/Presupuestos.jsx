8a33ff5af3a0522f53981a6a5fd3c20cfb754728
t.zemmari <tarik.zemmari@xabec.es>
2021-05-04 16:48:50 +0200

    added footer and npm styled-componentes installed

--- src/Containers/Presupuestos/Presupuestos.jsx
+++ src/Containers/Presupuestos/Presupuestos.jsx
@@ -13,9 +13,8 @@ import {connect} from 'react-redux';
 import axios from 'axios';
 import { useHistory } from 'react-router';
 import ContactRender from '../../components/Modal/Contact-render';
-
-
-
+import { Footer } from 'antd/lib/layout/layout';
+import FooterDos from '../../Footer-dos/Footer';
 
 
 const useStyles = makeStyles((theme)=>({
@@ -266,145 +265,137 @@ const RenderSamusungModels =(props)=>{
       </>
    )}else{
        return(
-       <>
-       
-           <div className="vista-presupuesto-container">
-            <Header style='repuestos'/>
-            <div className="body-container-prepusupuesto">
-                <div className="vista-imagen-promocional">
-                    <img src="" alt=""/>
-                </div>
+
+<>
+      
+      <div className="vista-presupuesto-container">
+      <Header style='repuestos'/>
+           <div className="body-container-prepusupuesto">
+               <div className="vista-imagen-promocional">
+                   <img src="" alt=""/>
+               </div>
 
 
-                {/*.................Div seleccion de marca y modelo ---------------*/}
+               {/*.................Div seleccion de marca y modelo ---------------*/}
 
-                <div className="vista-contenedor-marca-y-modelo">
+               <div className="vista-contenedor-marca-y-modelo">
 
-                <div className="vista-marca-modelo-solo-texto">
-                    Selecciona <span className="span-vista-presupuesto">Marca y Modelo</span>
-                    
-                </div>
+               <div className="vista-marca-modelo-solo-texto">
+                   Selecciona <span className="span-vista-presupuesto">Marca y Modelo</span>
+                   
+               </div>
 
-                <div className="vista-modelos-y-marca-inputs">
-                <div className="SelectInput">
-
-                    <FormControl className={classes.formControl}>
-
-                      
-                        <Select labelID ='selected' id = 'Marcas' value ={marca} onChange={handelMarca}>
-
-
-                           <MenuItem value ='Alcatel' >Alcatel</MenuItem> 
-                           <MenuItem value ='Samsung'>Samsung</MenuItem> 
-                           <MenuItem value ='Xiaomi'>Xiaomi</MenuItem> 
-                           <MenuItem value ='Iphone'>Iphone</MenuItem> 
-                           <MenuItem value = 'Lg'>Lg</MenuItem> 
-                           <MenuItem value = 'Sony'>Sony</MenuItem> 
-                           <MenuItem value = 'Nokia'>Nokia</MenuItem> 
-
-                        </Select>
-                       </FormControl>
-
-                       <FormControl className={classes.formControl}>
-
-                      
-                        <Select labelID ='select-demo' id = 'Modelos' value ={modelo} onChange={handelModel}>
-
-
-                           <MenuItem value ='Alcatel' >Alcatel</MenuItem> 
-                           <MenuItem value ='Samsung'>Samsung</MenuItem> 
-                           <MenuItem value ='Xiaomi'>Xiaomi</MenuItem> 
-                           <MenuItem value ='Iphone'>Iphone</MenuItem> 
-                           <MenuItem value = 'Lg'>Lg</MenuItem> 
-                           <MenuItem value = 'Sony'>Sony</MenuItem> 
-                           <MenuItem value = 'Nokia'>Nokia</MenuItem> 
-
-                        </Select>
-                       </FormControl>
-                </div>
-                </div>
-
-
-
-                </div>
-
-                 {/*.................Div contenedor div imagen-croquis / div elegir tipo de reparacion----// ---------------*/}
-
-
-                 <div className="vista-contenedor-div-croquis-div-reparaciones">
-
-                      <div className="vista-div-imagen-croquis">
-                          <div className="marca-y-modelo">
-                             Modelo :   <strong> {marca}  {modelo}</strong>
-                          </div>
-                          <div className="separador-presupuesto"></div>
-                          <img className ='imagen-style' src={Phone} alt="croquis"/>
-
-
-                      </div>
-
-                      <div className="vista-div-reparaciones">
-                        <div className="no-enciende-ni-carga"><h2>No enciende / No Carga </h2></div>
-                        <div className="vista-check">
-                            <input className='checkbox-style' type="checkbox"/>
-                            Conector de carga -
-                        </div>
-
-                        <div className="error-estetico"><h2>Daños estéticos / Roturas </h2></div>
-                        <div className="vista-check">
-                        <input className='checkbox-style' type="checkbox"/>
-                        Pantalla ORIGINAL - 
-
-                        </div>
-
-                        <div className="vista-div-problemas-de-audio"><h2>Problemas de audio </h2></div>
-                        <div className="vista-check vista-audio-check">
-                        
-
-                          <div className="tipo-averia-audio">
-                            <div className="div-checkbox-and-averia">
-                                
-                                <input className='checkbox-style' type="checkbox"/>
-                               <div className="altavoz">Micrófono -</div>
-                               </div>
-
-                            <div className="div-checkbox-and-averia">
-                            <input className='checkbox-style' type="checkbox"/>
-                            <div className="speaker">Auricular speaker -</div>
-                               </div>
-
-                            <div className="div-checkbox-and-averia">
-                               <input className='checkbox-style' type="checkbox"/>  
-                               <div className="altavoz-buzzer">Altavoz buzzer -</div>
-                            </div>
-
-                            </div>
-
-                            
-                        </div>
-
-                        <div className="error-de-software"><h2>Reinstalacion de softwares </h2></div>
-                        <div className="vista-check">
-                            <input className='checkbox-style' type="checkbox"/> 
-                            Software -
-                        </div>
-
-                        <div className="no-enciende-ni-carga"><h2>Contacta con nosotros si no encuentras tu averia </h2></div>
-                        <div className="vista-check">
-                         <div className="contacta-nos-style"> Contacta-Nos </div>
-                        </div>
-                      
-                      </div>
-                     
-
-
-                 </div>
-
-                      <div className="separador-presupuesto-vista-div-reparaciones"></div>
+               <div className="vista-modelos-y-marca-inputs">
+                 <RenderSamusungMarcas/>
+                 <RenderSamusungModels/>
 
                </div>
-           </div>
-        </>
+
+
+
+               </div>
+
+                {/*.................Div contenedor div imagen-croquis / div elegir tipo de reparacion----// ---------------*/}
+
+
+                <div className="vista-contenedor-div-croquis-div-reparaciones">
+
+                     <div className="vista-div-imagen-croquis">
+                         <div className="marca-y-modelo">
+                            Modelo :   <strong> {marca}  {modelo}</strong>
+                         </div>
+                         <div className="separador-presupuesto"></div>
+                         <img className ='imagen-style' src={Phone} alt="croquis"/>
+
+
+                     </div>
+
+                     <div className="vista-div-reparaciones">
+                       <div className="no-enciende-ni-carga"><h2>No enciende / No Carga </h2></div>
+                       <div className="vista-check">
+                           <input className='checkbox-style' type="checkbox"/>
+                          
+                           Conector de carga -  <strong> {resultado[0]?.conectorDeCarga} Euros</strong>
+                       </div>
+
+                       <div className="error-estetico"><h2>Daños estéticos / Roturas </h2></div>
+                       <div className="vista-check">
+                       <input className='checkbox-style' type="checkbox"/>
+                       Pantalla ORIGINAL -  <strong> {resultado[0]?.pantalla} Euros</strong>
+
+                       </div>
+
+                       <div className="vista-div-problemas-de-audio"><h2>Problemas de audio </h2></div>
+                       <div className="vista-check vista-audio-check">
+                       
+
+                         <div className="tipo-averia-audio">
+                           <div className="div-checkbox-and-averia">
+                               
+                               <input className='checkbox-style' type="checkbox"/>
+                              <div className="altavoz">Micrófono - <strong> {resultado[0]?.microfono} Euros</strong></div>
+                              </div>
+
+                           <div className="div-checkbox-and-averia">
+                           <input className='checkbox-style' type="checkbox"/>
+                           <div className="speaker">Auricular speaker - <strong> {resultado[0]?.auricular} Euros</strong> </div>
+                              </div>
+
+                           <div className="div-checkbox-and-averia">
+                              <input className='checkbox-style' type="checkbox"/>  
+                              <div className="altavoz-buzzer">Altavoz buzzer - <strong> {resultado[0]?.buzz} Euros</strong></div>
+                           </div>
+
+                           </div>
+
+                           
+                       </div>
+
+                       <div className="error-de-software"><h2>Reinstalacion de softwares </h2></div>
+                       <div className="vista-check">
+                           <input className='checkbox-style' type="checkbox"/> 
+                           Software -  <strong> {resultado[0]?.software} Euros</strong>
+                       </div>
+
+                       <div className="no-enciende-ni-carga"><h2>Contacta con nosotros si no encuentras tu averia </h2></div>
+                       <div className="vista-check">
+
+                           <ContactRender>
+                               <div className="contacta-nos-style" onClick={()=> history.push('/contact')}> Contacta-Nos </div>
+                          </ContactRender>
+                       </div>
+                     
+                     </div>
+                    
+
+
+                </div>
+
+                     <div className="separador-presupuesto-vista-div-reparaciones"></div>
+
+
+ 
+              </div>
+             
+        
+          </div>
+          <FooterDos/>
+     </>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
        )
            
        
