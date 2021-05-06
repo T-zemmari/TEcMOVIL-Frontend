ea6897e1648e5791f004336ae37a312380974e7a
t.zemmari <tarik.zemmari@xabec.es>
2021-05-06 18:24:12 +0200

    still working on lista-pedido

--- src/components/Lista-Pedido/Lista-pedido.jsx
+++ src/components/Lista-Pedido/Lista-pedido.jsx
@@ -2,14 +2,25 @@ import React from 'react';
 import './Lista-pedido.scss';
 import {connect} from 'react-redux';
 import Button from '@material-ui/core/Button'
+import { LensTwoTone } from '@material-ui/icons';
 
 
 const ListaPedidos =(props)=>{
 
   const carrito = props.carrito;
   const user = props.user;
+  console.log(carrito.name)
 
 
+  
+
+
+  let  array = carrito;
+
+  let precioTotal = array.reduce((sum, value) => ( sum + parseInt(value.price) ), 0);
+
+
+ 
     return (
 
         <div className="lista-pedidos-container">
@@ -20,8 +31,29 @@ const ListaPedidos =(props)=>{
                 </div>
                 <div className="contenedor-lista-pedidos-mas-precio-final">
                     <div className ='lista-productos-eligidos'>
-
+                    <Button variant="outlined" color="default">
+                     Lista De Productos
+                    </Button>
                     </div>
+
+                    <div className="lista-detallada-de-productos">
+                        {carrito.map((element,_id) => (<div key={_id}>
+                               
+                                <div className="nombre-del-producto">
+                                    Producto : {element.name}
+                                </div>
+                                <div className="precio-por-unidad">
+                                    Precio /Unidad  : {element.price}
+                                </div>
+
+                            
+                            
+                            </div> ))}
+                    </div>
+                    <div className="precio-total-productos">
+                        Precio Total :{precioTotal} Euros
+                    </div>
+
                 </div>
             </div>
 
